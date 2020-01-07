58fba0eeab3418f4ae7dd05c96981830d58342ab
dev.darshana991 <dev.darshana92@gmail.com>
2020-01-08 01:16:56 +0530

    added the hero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartComponent } from './cart/cart.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CartComponent
+    CartComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
